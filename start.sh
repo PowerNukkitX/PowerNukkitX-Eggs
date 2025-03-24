a9a79955f099e13148ba1b2d2997673e21925f2a
KeksDev <54219265+tunikakeks@users.noreply.github.com>
2025-03-24 13:35:19 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,2 @@
+wget https://github.com/PowerNukkitX/PowerNukkitX/releases/download/snapshot/powernukkitx-shaded.jar
+java -Dfile.encoding=UTF-8 -Djansi.passthrough=true -Dterminal.ansi=true -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+EnableJVMCI -jar powernukkitx-shaded.jar
