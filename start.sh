52674bd54201e1d4eb5898c639b3799ede09bd1f
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2026-01-09 22:27:11 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-wget -O powernukkitx-shaded.jar -o /dev/null https://github.com/PowerNukkitX/PowerNukkitX/releases/download/snapshot/powernukkitx-shaded.jar
-java -Dfile.encoding=UTF-8 -Djansi.passthrough=true -Dterminal.ansi=true -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+EnableJVMCI -XX:MaxRAMPercentage=95.0 -jar powernukkitx-shaded.jar
+wget -O powernukkitx-shaded.jar -o /dev/null https://github.com/PowerNukkitX/PowerNukkitX/releases/download/snapshot/powernukkitx.jar
+java -Dfile.encoding=UTF-8 -Djansi.passthrough=true -Dterminal.ansi=true -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+EnableJVMCI -XX:MaxRAMPercentage=95.0 -jar powernukkitx.jar
